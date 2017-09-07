ddaa9bf9ea9d1b2d179028d01141d8177018c68e
Tales Ebert <djtalesdj@universo.univates.br>
2017-09-06 21:35:42 -0300

    ]Init

---
+++ platforms/browser/platform_www/cordova_plugins.js
@@ -0,0 +1,18 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-camera-preview/www/CameraPreview.js",
+        "id": "cordova-plugin-camera-preview.CameraPreview",
+        "pluginId": "cordova-plugin-camera-preview",
+        "clobbers": [
+            "CameraPreview"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-camera-preview": "0.9.0"
+}
+// BOTTOM OF METADATA
+});
